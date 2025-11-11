21fa64e7d6294a3cdc76c8d448b440867816d85f
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2025-11-11 23:20:40 +0600

    project setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { onMounted } from 'vue';
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+onMounted(()=>{
+  AOS.init();
+})
+</script>
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+</template>
+<style scoped>
+
+</style>
