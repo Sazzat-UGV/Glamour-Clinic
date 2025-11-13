a86ac5a9165a14b72a18742da7037cf21b4ec54c
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2025-11-13 23:20:57 +0600

    service section completed

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css'
 import Header from './components/Header.vue';
 import Hero from './components/Hero.vue';
+import Service from './components/Service.vue';
 onMounted(() => {
   AOS.init();
 })
@@ -11,5 +12,6 @@ onMounted(() => {
 <template>
   <Header />
   <Hero />
+  <Service />
 </template>
 <style scoped></style>
